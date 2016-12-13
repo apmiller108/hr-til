f21b4117a7ae0309a9a5f8d52f779fc9111afcd8
Alex Miller <apmiller108@yahoo.com>
2016-12-12 22:57:46 -0500

    Create production seed for channels.

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,6 +1,3 @@
-# To download production data instead of these seeds, run
-# rake db:restore_production_dump
-
 channels = [
   { name: 'Angular2', twitter_hashtag: 'angular2' },
   { name: 'command-line', twitter_hashtag: 'commandline' },
@@ -19,31 +16,6 @@ channels = [
   { name: 'workflow', twitter_hashtag: 'workflow' },
 ]
 
-print "Creating #{channels.length} channels"
 channels.each do |channel|
   Channel.find_or_create_by!(name: channel[:name], twitter_hashtag: channel[:twitter_hashtag])
 end
-puts " ...done."
-
-print "Creating developers"
-5.times do
-  username = Phil.name.downcase.delete(' ')
-  Developer.create(username: username, email: "#{username}@alex-miller.co")
-end
-puts " ...done."
-
-print "Creating posts"
-40.times do
-  channel = Channel.all.sample
-  likes = rand(1..20)
-
-  Post.create!(body: Phil.words(100).capitalize,
-    channel: channel,
-    developer: Developer.all.sample,
-    title: Phil.words(2).capitalize,
-    likes: likes,
-    max_likes: likes,
-    created_at: Date.today - rand(30).days,
-    published_at: [(Date.today - rand(30).days), nil].sample)
-end
-puts " ...done."
